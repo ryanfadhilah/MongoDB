c7e11a146fdbd86413536cfa08e1a8386bc46835
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-18 09:19:33 +0700

    erase

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const express = require('express')
-const cors = require('cors')
-
-const PORT = 4400
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.status(200).send('<h1> Integrated MongoDb with Express </h1>')
-})
-
-app.listen(PORT, () => {
-    console.log(`MongoDB API Runnint at PORT: ${PORT}`)
-})
+test
