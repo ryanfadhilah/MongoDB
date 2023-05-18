274e9105a57872c72c9554430dc2ad89e1c520bb
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-18 18:25:14 +0700

    COMPLETE ^_^

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express()
 PORT = 3000
 
 
-
-
 /////////////////////////////////////
 // Middleware
 app.use(express.json())
@@ -69,7 +67,17 @@ app.put('/customer/:id', async (req, res) => {
 })
 
 // DELETE
+app.delete('/customer/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const customer = await Customer.findByIdAndDelete(id, req.body)
 
+        res.status(200).json(customer)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 /////////////////////////////////////
 // API SETUP
