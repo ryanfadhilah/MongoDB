eb61e797bacda8257d3fa8abb5fa8eab76f35c41
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-18 18:15:08 +0700

    UPDATE Route

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const Customer = require('./models/customer_model')
 const app = express()
 PORT = 3000
 
+
+
+
 /////////////////////////////////////
 // Middleware
 app.use(express.json())
+// BONUS kalau mau update pake form (usualy JSON)
+app.use(express.urlencoded({ extended: false }))
+
 
 /////////////////////////////////////
 // ROUTES 30:32
@@ -46,7 +52,21 @@ app.get('/customer/:id', async (req, res) => {
 })
 
 // UPDATE
+app.put('/customer/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const customer = await Customer.findByIdAndUpdate(id, req.body)
+        // jika data Customer tidak di update
+        if (!customer) {
+            return res.status(404).json({ message: `cannot find product with ID: ${id}` })
+        }
 
+        const updated_customer = await Customer.findById(id)
+        res.status(200).json(updated_customer)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 // DELETE
 
